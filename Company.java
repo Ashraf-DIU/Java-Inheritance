59951592f55b9d8843d3892306b8b4c92653a8e8
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-10-04 15:46:16 +0600

    adding company

---
+++ Company.java
@@ -0,0 +1,55 @@
+
+// Parent class Employee
+class Employee {
+
+    private int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public void work() {
+        System.out.println("working as an employee!");
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+} 
+
+// Child class HRManager
+class HRManager extends Employee {
+
+    public HRManager(int salary) {
+        super(salary);
+    }
+
+    public void work() {
+        System.out.println("\nManaging employees");
+    }
+
+    public void addEmployee() {
+        System.out.println("\nAdding new employee!");
+    }
+}
+
+// Main class
+public class Company {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee(40000);
+
+        HRManager mgr = new HRManager(70000);
+
+        emp.work();
+
+        System.out.println("Employee salary: " + emp.getSalary());
+
+        mgr.work();
+
+        System.out.println("Manager salary: " + mgr.getSalary());
+
+        mgr.addEmployee();
+    }
+}
+
